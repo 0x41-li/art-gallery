5d7ff7626703d0c6d52e165843f2d58b8aba6b16
Ali Sbane <ali.sbane.dev@gmail.com>
2022-08-28 12:22:13 +0100

    Desktop: Home page's gallery section completed

--- src/pages/home/sections/Gallery.jsx
+++ src/pages/home/sections/Gallery.jsx
@@ -4,32 +4,40 @@ import { useMediaQuery } from "../../../hooks/useMediaQuery";
 // images
 import greyRoomWithPaintings343w from "../../../assets/images/home/gallery/grey-room-with-paintings-343w.jpg";
 import greyRoomWithPaintings398w from "../../../assets/images/home/gallery/grey-room-with-paintings-398w.jpg";
+import greyRoomWithPaintings635w from "../../../assets/images/home/gallery/grey-room-with-paintings-635w.jpg";
 
 import greyRoomWithATableAndAPainting343w from "../../../assets/images/home/gallery/grey-room-with-a-table-and-a-painting-343w.jpg";
 import greyRoomWithATableAndAPainting398w from "../../../assets/images/home/gallery/grey-room-with-a-table-and-a-painting-398w.jpg";
+import greyRoomWithATableAndAPainting635w from "../../../assets/images/home/gallery/grey-room-with-a-table-and-a-painting-635w.jpg";
 
 import brownRoomWithPeopleLookingAtPaintings343w from "../../../assets/images/home/gallery/brown-room-with-people-looking-at-paintings-343w.jpg";
 import brownRoomWithPeopleLookingAtPaintings398w from "../../../assets/images/home/gallery/brown-room-with-people-looking-at-paintings-398w.jpg";
+import brownRoomWithPeopleLookingAtPaintings445w from "../../../assets/images/home/gallery/brown-room-with-people-looking-at-paintings-445w.jpg";
 
 const Gallery = () => {
   const isTablet = useMediaQuery("(min-width: 768px)");
+  const isDesktop = useMediaQuery("(min-width: 1280px)");
 
   return (
-    <section className="px-4 mt-[120px] max-w-[375px] mx-auto md:grid md:max-w-[768px] md:grid-cols-[223px_46px_107px_280px] md:grid-rows-[400px_313px_396px] md:pl-[39px] md:pr-[40px] md:gap-[11px]">
+    <section className="px-4 mt-[120px] max-w-[375px] mx-auto md:grid md:max-w-[768px] md:grid-cols-[223px_46px_107px_280px] md:grid-rows-[400px_313px_396px] md:pl-[39px] md:pr-[40px] md:gap-[11px] xl:max-w-[1110px] xl:px-0 xl:grid-cols-[350px_65px_160px_445px] xl:grid-rows-[400px_313px_377px] xl:gap-[30px] xl:mt-[180px]">
       <div
         style={{
           backgroundImage: `url(${
-            isTablet ? greyRoomWithPaintings398w : greyRoomWithPaintings343w
+            isDesktop
+              ? greyRoomWithPaintings635w
+              : isTablet
+              ? greyRoomWithPaintings398w
+              : greyRoomWithPaintings343w
           })`,
         }}
         className="h-[320px] w-[343px] bg-cover bg-no-repeat md:col-start-3 md:col-end-5 md:w-full md:h-full"
       ></div>
-      <div className="mt-[25px] md:w-[223px] md:col-start-1 md:row-start-1 md:mt-[36px]">
-        <h2 className="font-bsd [font-size:50px] [font-weight:900] [line-height:45px] uppercase">
+      <div className="mt-[25px] md:col-start-1 md:row-start-1 md:mt-[36px] xl:mt-[60px]">
+        <h2 className="font-bsd [font-size:50px] [font-weight:900] [line-height:45px] uppercase xl:[font-size:60px] xl:[line-height:60px]">
           Your Day
-          <br /> at the Gallery
+          <br className="xl:hidden" /> at the Gallery
         </h2>
-        <p className="mt-[21px] font-outfit [font-size:18px] [line-height:28px] [font-weight:300] text-dark-grey md:mt-[24px]">
+        <p className="mt-[21px] font-outfit [font-size:18px] [line-height:28px] [font-weight:300] text-dark-grey md:mt-[24px] xl:[font-size:22px] xl:[line-height:32px] xl:mt-[32px]">
           Wander through our distinct collections and find new insights about
           our artists. Dive into the details of their creative process.
         </p>
@@ -37,7 +45,9 @@ const Gallery = () => {
       <div
         style={{
           backgroundImage: `url(${
-            isTablet
+            isDesktop
+              ? greyRoomWithATableAndAPainting635w
+              : isTablet
               ? greyRoomWithATableAndAPainting398w
               : greyRoomWithATableAndAPainting343w
           })`,
@@ -47,18 +57,20 @@ const Gallery = () => {
       <div
         style={{
           backgroundImage: `url(${
-            isTablet
+            isDesktop
+              ? brownRoomWithPeopleLookingAtPaintings445w
+              : isTablet
               ? brownRoomWithPeopleLookingAtPaintings398w
               : brownRoomWithPeopleLookingAtPaintings343w
           })`,
         }}
         className="h-[200px] w-[343px] bg-cover bg-no-repeat mt-[16px] md:mt-0 md:h-full md:w-full md:row-start-2 md:row-end-3 md:col-start-4 md:col-end-5"
       ></div>
-      <div className="px-[24px] pt-[48px] pb-[47px] bg-almost-black mt-[16px] md:mt-0 md:pt-[85px] md:pl-[29px] md:pr-[28px]">
-        <h2 className="font-bsd text-white [font-size:50px] [line-height:45px] [font-weight:900]">
+      <div className="px-[24px] pt-[48px] pb-[47px] bg-almost-black mt-[16px] md:mt-0 md:pt-[85px] md:pl-[29px] md:pr-[28px] xl:pt-[65px] xl:pb-[64px] xl:pl-[48px] xl:pr-[47px]">
+        <h2 className="font-bsd text-white [font-size:50px] [line-height:45px] [font-weight:900] xl:[font-size:60px] xl:[line-height:60px]">
           COME & BE <br /> INSPIRED
         </h2>
-        <p className="mt-[24px] font-outfit text-white [font-size:18px] [line-height:28px] [font-weight:300]">
+        <p className="mt-[24px] font-outfit text-white [font-size:18px] [line-height:28px] [font-weight:300] xl:[font-size:22px] xl:[line-height:32px] xl:mt-[32px]">
           We’re excited to welcome you to our gallery and see how our
           collections influence you.
         </p>
