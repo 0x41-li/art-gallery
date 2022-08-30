93de6bbe7edeaea813b3721c05602f0ef02d63c6
Ali Sbane <ali.sbane.dev@gmail.com>
2022-08-30 10:40:16 +0100

    Secondary footer for location page added

--- src/partial/Footer.jsx
+++ src/partial/Footer.jsx
@@ -7,19 +7,32 @@ import { ReactComponent as InstagramIcon } from "../assets/icons/instagram-icon.
 import { ReactComponent as TwitterIcon } from "../assets/icons/twitter-icon.svg";
 
 const Footer = (props) => {
-  const location = useLocation();
   return (
     <footer
       className={
         props.addToClassName +
-        " px-[32px] py-[48px] bg-almost-black md:py-[56px] md:pl-[39px] md:pr-[40px] xl:px-0 xl:py-[80px]"
+        " px-[32px] py-[48px]  md:py-[56px] md:pl-[39px] md:pr-[40px] xl:px-0 xl:py-[80px]" +
+        " " +
+        (props.secondary ? "bg-gold" : "bg-almost-black")
       }
     >
       <div className="max-w-[1110px] mx-auto md:flex">
         <Link to="/" className="h-[40px]">
-          <Logo className="h-[40px] w-[111px] xl:w-[160px] xl:h-[58px] text-white" />
+          <Logo
+            className={
+              "h-[40px] w-[111px] xl:w-[160px] xl:h-[58px]" +
+              " " +
+              (props.secondary ? "text-almost-black" : "text-white")
+            }
+          />
         </Link>
-        <p className="mt-[38px] text-white font-outfit [font-size:16px] [line-height:26px] max-w-[443px] md:max-w-none md:mt-0 md:[flex:0_0_36.6vw] md:ml-[8.5vw] xl:[font-size:18px] xl:[line-height:28px] xl:[flex:0_0_430px] xl:ml-[140px]">
+        <p
+          className={
+            "mt-[38px] font-outfit [font-size:16px] [line-height:26px] max-w-[443px] md:max-w-none md:mt-0 md:[flex:0_0_36.6vw] md:ml-[8.5vw] xl:[font-size:18px] xl:[line-height:28px] xl:[flex:0_0_430px] xl:ml-[140px]" +
+            " " +
+            (props.secondary ? "text-almost-black" : "text-white")
+          }
+        >
           The Modern Art Gallery is free to all visitors and open seven days a
           week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
         </p>
@@ -28,19 +41,37 @@ const Footer = (props) => {
           <div className="flex gap-[20px]">
             <a
               href="#"
-              className="group h-[20px] text-white hover:text-gold [transition:color_0.4s]"
+              className={
+                "h-[20px]  [transition:color_0.4s]" +
+                " " +
+                (props.secondary
+                  ? "text-almost-black hover:text-white"
+                  : "text-white hover:text-gold")
+              }
             >
               <FacebookIcon className="fill-current" />
             </a>
             <a
               href="#"
-              className="group h-[20px] text-white hover:text-gold [transition:color_0.4s]"
+              className={
+                "h-[20px]  [transition:color_0.4s]" +
+                " " +
+                (props.secondary
+                  ? "text-almost-black hover:text-white"
+                  : "text-white hover:text-gold")
+              }
             >
               <InstagramIcon className="fill-current" />
             </a>
             <a
               href="#"
-              className="group h-[20px] text-white hover:text-gold [transition:color_0.4s]"
+              className={
+                "h-[20px]  [transition:color_0.4s]" +
+                " " +
+                (props.secondary
+                  ? "text-almost-black hover:text-white"
+                  : "text-white hover:text-gold")
+              }
             >
               <TwitterIcon className="fill-current" />
             </a>
