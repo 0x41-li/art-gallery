e07320c30c94671f410e1a88525fe408feecca0d
Ali Sbane <ali.sbane.dev@gmail.com>
2022-08-31 07:56:19 +0100

    Tablet: location page's our location section completed

--- src/pages/location/sections/OurLocation.jsx
+++ src/pages/location/sections/OurLocation.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const OurLocation = () => {
   return (
-    <section className="">
-      <div className="bg-almost-black px-[16px] pt-[48px] pb-[56px]">
-        <div className="">
-          <h2 className="font-bsd [font-size:50px] [line-height:45px] [font-weight:900] text-white uppercase">
+    <section className="bg-almost-black">
+      <div className="px-[16px] pt-[48px] pb-[56px] md:pl-[39px] md:pr-[40px] md:pt-[88px] pb-[80px] md:flex md:justify-between md:max-w-[900px] md:mx-auto">
+        <div className="[flex:0_0_223px]">
+          <h2 className="font-bsd [font-size:50px] [line-height:45px] [font-weight:900] text-white uppercase  md:[font-size:55px] md:[line-height:50px]">
             OUR LOCATION
           </h2>
         </div>
-        <div className="">
-          <h3 className="font-bsd [font-size:32px] [line-height:32px] [font-weight:900] text-gold uppercase mt-[48px]">
+        <div className="mt-[48px] md:mt-0">
+          <h3 className="font-bsd [font-size:32px] [line-height:32px] [font-weight:900] text-gold uppercase">
             99 King Street
           </h3>
           <address className="mt-[20px] text-white font-outfit [font-size:18px] [line-height:28px] [font-weight:300]">
