4c9203312682575b7f9210d8590ee7353d64a758
Ali Sbane <ali.sbane.dev@gmail.com>
2022-08-31 13:53:33 +0100

    Adding code-spliting, and lazy-loading for the pages' components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Location from "./pages/location/Location";
 
 // css
 import "./index.css";
 
+const Location = React.lazy(() => import("./pages/location/Location"));
+const Home = React.lazy(() => import("./pages/home/Home"));
+
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/location" element={<Location />} />
-      </Routes>
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/location" element={<Location />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
