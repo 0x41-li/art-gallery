644404df8d28eccb4f1d18ce2ae0ef72ba0a1763
Ali Sbane <ali.sbane.dev@gmail.com>
2022-09-04 10:48:35 +0100

    Replacing the hero section's main image which was a background image with an actual image element, and that's because it's a main component of the page and need to be accessible

--- src/pages/home/sections/Hero.jsx
+++ src/pages/home/sections/Hero.jsx
@@ -5,11 +5,14 @@ import RedirectingBtn from "../../../components/RedirectingBtn";
 const Hero = () => {
   return (
     <section className="relative max-w-[1440px] mx-auto">
-      <div
-        style={{ backgroundImage: `url(${heroImage})` }}
-        className="h-[64vw] bg-cover bg-no-repeat [background-position:1px_-42vw] md:h-[700px] md:w-[57vw] md:[background-position:-18px_0px] lg:[background-position:-18px_-16vw] xl:bg-almost-black xl:[background-position:top_0px_right_0px] xl:[background-size:540px_800px] xl:w-[990px] xl:h-[800px]"
-      ></div>
-      <div className="px-4 max-w-[475px] md:px-0 md:max-w-[339px] md:absolute md:top-[145px] md:left-[51vw] xl:w-[1110px] xl:max-w-none xl:left-[165px] xl:flex xl:top-[190px]">
+      <div className="relative overflow-hidden h-[64vw] md:h-[700px] md:w-[56.9vw] xl:bg-almost-black xl:w-[990px] xl:h-[800px]">
+        <img
+          src={heroImage}
+          alt="Two people looking on a painting"
+          className="max-w-full absolute top-[-160px] right-0 md:h-[91.1vw] md:top-[0px] md:w-f[56vw] lg:top-[-16vw] xl:w-[540px] xl:h-[800px] xl:top-0"
+        />
+      </div>
+      <div className="px-4 max-w-[475px] md:px-0 md:max-w-[339px] md:absolute md:top-[145px] md:left-[50.6vw] xl:w-[1110px] xl:max-w-none xl:left-[165px] xl:flex xl:top-[190px]">
         <div className="relative mt-8 md:mt-0 xl:w-[514px]">
           <div className="relative z-10 xl:w-[285px] xl:overflow-hidden">
             <h1 className="text-almost-black font-bsd [font-size:60px] [font-weight:900] [line-height:55px] uppercase md:[font-size:70px] md:[line-height:65px] xl:[font-size:96px] xl:[line-height:88px] xl:w-[514px] xl:text-white">
