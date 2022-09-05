1e45b1e50e12eebc99b1f49401858cd19817cd9b
Ali Sbane <ali.sbane.dev@gmail.com>
2022-09-05 18:01:46 +0100

    Trying mix-blend-mode property

--- src/pages/home/sections/Hero.jsx
+++ src/pages/home/sections/Hero.jsx
@@ -14,14 +14,7 @@ const Hero = () => {
       </div>
       <div className="px-4 max-w-[475px] md:px-0 md:max-w-[339px] md:absolute md:top-[145px] md:left-[50.6vw] xl:w-[1110px] xl:max-w-none xl:left-[165px] xl:flex xl:top-[190px]">
         <div className="relative mt-8 md:mt-0 xl:w-[514px]">
-          <div className="relative z-10 xl:w-[285px] xl:overflow-hidden">
-            <h1 className="text-almost-black font-bsd [font-size:60px] [font-weight:900] [line-height:55px] uppercase md:[font-size:70px] md:[line-height:65px] xl:[font-size:96px] xl:[line-height:88px] xl:w-[514px] xl:text-white">
-              MODERN <br />
-              ART GALLERY
-            </h1>
-          </div>
-
-          <h1 className="hidden z-0 text-almost-black absolute font-bsd [font-size:96px] [line-height:88px] [font-weight:900] top-0 left-0 xl:block">
+          <h1 className="text-almost-black font-bsd [font-size:60px] [font-weight:900] [line-height:55px] uppercase md:[font-size:70px] md:[line-height:65px] xl:[font-size:96px] xl:[line-height:88px] xl:w-[514px] xl:text-white [mix-blend-mode:difference]">
             MODERN <br />
             ART GALLERY
           </h1>
